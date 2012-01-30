4b44763995e3bab9d0603cd3befa950421efd4aa
Nada Amin <namin@alum.mit.edu>
2012-01-30 15:57:56 +0100

    fix type errors in ordered_pres lemma

--- src/coq/dot/meta_mini_trans.v
+++ src/coq/dot/meta_mini_trans.v
@@ -13,11 +13,11 @@ Hint Unfold transitivity_on.
 
 (* TODO: requires well-kindedness of environment *)
 Lemma ordered_pres : 
-  (forall (E : env) (p : tm) (T : tp) (H : E |= p ~:% T)  T', path p -> E |= p ~:% T' -> ordered_tp T T') /\
-  (forall (E : env) (H : wf_env_mini e), True) /\
+  (forall (E : env) (p : tm) (T : tp) (H : E |= p ~:% T)  T', path p -> E |= p ~:% T' -> ordered_tp E T T') /\
+  (forall (E : env) (H : wf_env_mini E), True) /\
   (forall (E : env) (T1 T2 : tp) (H : E |= T1 ~<% T2) T1' T2', E |= T1' ~<% T2' -> ordered_tp E T1 T1' -> ordered_tp E T2 T2') /\
   (forall (E : env) (D1 D2 : decl) (H : sub_decl_mini E D1 D2) D1' D2', sub_decl_mini E D1' D2' -> ordered_decl E D1 D1' -> ordered_decl E D2 D2') /\
-  (forall (E : env) (T : tp) (H : wf_tp_mini E T), True)
+  (forall (E : env) (T : tp) (H : wf_tp_mini E T), True).
 
 Proof.
 Check typing_mini_mutind.
