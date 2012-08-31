88bc64e7de14b0e60312967f34e8a953ad4f7591
Nada Amin <namin@alum.mit.edu>
2012-08-31 02:34:57 +0200

    simpler (too?) tribe emulation

--- doc/candidacy/tribe.scala
+++ doc/candidacy/tribe.scala
@@ -1,43 +1,23 @@
-abstract class Graph { graph =>
-  type Node <: ANode
-  type Edge <: AEdge
-
-  def newNode(): Node
-  def newEdge(from: Node, to: Node): Edge
-
+class Graph { graph =>
   class WithOut {
     val out: graph.type = graph
   }
 
-  abstract class ANode extends WithOut { this: Node =>
+  class Node extends WithOut {
     def connect(other: out.Node): out.Edge =
-      newEdge(this, other)
+      new out.Edge(this, other)
   }
 
-  abstract class AEdge extends WithOut { this: Edge =>
-    val from: out.Node
-    val to: out.Node
+  class Edge(f: Node, t: Node) extends WithOut {
+    val from: out.Node = f
+    val to: out.Node = t
   }
-}
-
-class BasicGraph extends Graph {
-  class Node extends ANode
-  class Edge(val from: Node, val to: Node) extends AEdge
-
-  def newNode(): Node =
-    new Node()
-  def newEdge(from: Node, to: Node): Edge =
-    new Edge(from, to)
 }
 
 class ColouredGraph extends Graph {
-  class Node(var colour: String) extends ANode
-  class Edge(val from: Node, val to: Node) extends AEdge
-
-  def newNode(): Node =
-    new Node("blue")
-  def newEdge(from: Node, to: Node): Edge =
-    new Edge(from, to)
+  class Node extends super.Node {
+    var colour: String = "blue"
+  }
 }
 
 object Library {
@@ -46,27 +26,24 @@ object Library {
     0 // ...
   }
   def copyEdge(e: Graph#Edge): e.out.Edge =
-    e.out.newEdge(e.from, e.to)
+    new e.out.Edge(e.from, e.to)
 
   def distance2(g: Graph)(n1: g.Node, n2: g.Node): Int = {
     val e: g.Edge = n1.connect(n2)
     0 // ...
   }
   def copyEdge2(g: Graph)(e: g.Edge): g.Edge =
-    g.newEdge(e.from, e.to)
+    new g.Edge(e.from, e.to)
 }
 
 object Test extends App {
   val cg1, cg2 = new ColouredGraph()
-  val cn1, cn3 = cg1.newNode()
-  val cn2 = cg2.newNode()
+  val cn1, cn3 = new cg1.Node()
+  val cn2 = new cg2.Node()
   val e = cn1.connect(cn3)
+  // this is a type mismatch
   // cn2.connect(cn3)
-  // error: type mismatch;
-  // found   : Test.cg1.Node
-  //  (which expands to)  Test.cg1.ColouredNode
-  // required: Test.cg2.Node
-  //  (which expands to)  Test.cg2.ColouredNode
+
   val d1 = Library.distance(cn1)(cn3)
   val d2 = Library.distance2(cg1)(cn1, cn3)
   val e1 = Library.copyEdge(e)
