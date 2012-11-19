3a488fd95e1f43f269dad5b2475e908850a228d2
Nada Amin <namin@alum.mit.edu>
2012-11-19 09:06:20 +0100

    pseudo freshness handling

--- src/scala/src/main/scala/dot/DotTyper.scala
+++ src/scala/src/main/scala/dot/DotTyper.scala
@@ -1,6 +1,6 @@
 package dot
 
-trait Constraints extends StandardTyperMonad {
+trait Constraints extends StandardTyperMonad with NominalBindingSyntax {
   override type State = Set[Constraint]
   override val initState = Set[Constraint]()
 
@@ -8,15 +8,24 @@ trait Constraints extends StandardTyperMonad {
     from.mapStateTo({state => state + c}, {state => Success(())})
   }
 
-  implicit def constraintExpected(tp1: Expected[Type]) = new {
+  implicit def subtypingConstraintExpected(tp1: Expected[Type]) = new {
     def <:<(tp2: Expected[Type]): Constraint = new SubtypeConstraint(tp1, tp2)
+  }
+
+  implicit def freshnessConstraintExpected[T: ContainsBinders](tp: Expected[T]) = new {
+    def fresh(x: Name): Constraint = new FreshnessConstraint(x, tp)
   }
 
   trait Constraint
   case class SubtypeConstraint(tv1: Expected[Type], tv2: Expected[Type]) extends Constraint
+  case class FreshnessConstraint[T: ContainsBinders](x: Name, nom: Expected[T]) extends Constraint
 }
 
-trait DotTyper extends StandardTyperMonad with Constraints with DotTyperSyntax with DotNominalSyntax with DotSubstitution {
+trait DotTyper extends StandardTyperMonad with DotTyperSyntax with DotNominalSyntax with DotSubstitution {
+  // NOTE: don't use constraints for now (baby steps)
+  override type State = Option[Nothing]
+  override val initState = None
+
   import Terms._
   import Types._
   import Members._
@@ -46,7 +55,9 @@ trait DotTyper extends StandardTyperMonad with Constraints with DotTyperSyntax w
 	_ <- assume(x, tc){for(
 	  ds <- expand(x, tc);
 	  _ <- wfCtorMems(ds, args);
-	  _ <- ofT(b, pt)) yield ()}) yield ()
+	  _ <- ofT(b, pt);
+	  tp <- !pt;
+	  if tp fresh(x)) yield ()}) yield ()
     }
   }
 
