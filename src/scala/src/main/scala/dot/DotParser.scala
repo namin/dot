8429a98e923c050bc959a955b9f429d209bb6f22
Nada Amin <namin@alum.mit.edu>
2012-11-20 23:01:57 +0100

    Parser for DOT.

---
+++ src/scala/src/main/scala/dot/DotParser.scala
@@ -0,0 +1,100 @@
+package dot
+
+import scala.util.parsing.combinator.syntactical.StdTokenParsers
+import scala.util.parsing.combinator.lexical.StdLexical
+import scala.collection.immutable._
+import util.parsing.combinator.{PackratParsers, ImplicitConversions}
+
+trait DotParsing extends StdTokenParsers with BindingParsers with PackratParsers with DotNominalSyntax with ImplicitConversions with Debug { theParser =>
+  type Tokens = StdLexical; val lexical = new StdLexical
+  lexical.delimiters ++= List("=", ";", ".", "(", ")", "{", "}", "=>", "⇒", ":", "..", "->", "→", "!", "&", "∧", "/\\", "|", "∨", "\\/", "⊥", "⊤")
+  lexical.reserved ++= List("val", "new", "Top", "Bottom", "Bot")
+
+  type P[T] = PackratParser[T]
+
+  private var indent = ""
+  def l[T](p: => Parser[T])(name: String): P[T] = Parser{ in =>
+    debug(indent + "trying " + name)
+    val oldIndent = indent
+    indent += " "
+    val r = p(in)
+    indent = oldIndent
+    debug(indent + name + " --> " + r)
+    r
+  }
+
+  import Terms._
+  import Types._
+  import Members._
+
+  def BindingParser(env: Map[String, Name]): BindingParser = new BindingParser(env)
+  class BindingParser(env: Map[String, Name]) extends BindingParserCore(env) {
+    def suggest[T](parser: Parser[T], pred: T => Boolean, err: T => String): Parser[T] = Parser{ in =>
+     parser(in) filterWithError(pred, err, in)
+    }
+    def only[T](parser: Parser[T], pred: T => Boolean, err: T => String): Parser[T] =
+      parser.flatMap{r => if (pred(r)) success(r) else failure(err(r))}
+    def lowerident(kind: String): P[String] = suggest[String](
+        ident.withFailureMessage(kind+" expected"),
+        _.charAt(0).isLower,
+        kind+" should start with a lower case, unlike "+_)
+    def upperident(kind: String, caveat: String = ""): P[String] = suggest[String](
+        ident.withFailureMessage(kind+ " expected"),
+        _.charAt(0).isUpper,
+        kind+" should start with an upper case"+caveat+", unlike "+_)
+    lazy val variable: P[Var] = l(bound(ident.withFailureMessage("variable expected")) ^^ Var) ("var")
+    lazy val valueLabel: P[ValueLabel] = l(lowerident("value label") ^^ ValueLabel) ("value label")
+    lazy val methodLabel: P[MethodLabel] = l(ident.withFailureMessage("method label expected") ^^ MethodLabel) ("method label")
+    lazy val value: P[Value] = variable
+    lazy val defn: P[Defn] =
+      l(methodLabel ~ ("(" ~> bind(ident) >> {x => ")" ~> "=" ~>
+        under(x){_.term}}) ^^ {case l~b => MethodDef(l, Method(b))}) ("method initialization") |
+      l(valueLabel ~ ("=" ~> value) ^^ {case l~v => ValueDef(l, v)}) ("field initialization")
+    lazy val defs: P[Defs] = l(
+      opt("(" ~> only[List[Defn]](repsep(defn, opt(";")), uniqLabels, _ => "duplicate definition") <~ ")") ^^ { x =>
+      x.map(Defs).getOrElse(Defs(List())) }) ("initialization")
+    lazy val newInstance: P[New] = l(
+      "val" ~> bind(ident) >> {x => "=" ~> "new" ~> concrete_typ ~
+       under(x){p => p.defs ~ (opt(";") ~> p.term <~ opt(";")) ^^ { case ds~t => (ds,t) }} ^^
+       { case tyc~args_scope => New(tyc, args_scope) }}) ("new instance")
+    lazy val term: P[Term] =
+      l(term ~ ("." ~> methodLabel) ~ ("(" ~> term <~ ")") ^^ {case o~m~a => Msel(o, m, a)}) ("method invocation") |
+      l(term ~ ("." ~> valueLabel) ^^ {case t~l => Sel(t, l)}) ("field selection") |
+      newInstance |
+      value |
+      l("(" ~> term <~ ")") ("parenthesized term")
+
+    lazy val abstractTypeLabel: P[AbstractTypeLabel] =
+      l(upperident("abstract type label") ^^ AbstractTypeLabel) ("abstract type label")
+    // TODO: check that class labels are only declared once
+    lazy val classLabel: P[ClassLabel] =
+      l("!" ~> upperident("class label", " after the question mark") ^^ ClassLabel) ("class label")
+    lazy val typeLabel: P[TypeLabel] = classLabel | abstractTypeLabel
+    lazy val top: P[Type] = l(("Top" | "⊤") ^^^ Top ) ("top type")
+    lazy val bottom: P[Type] = l(("Bottom" | "Bot" | "⊥") ^^^ Bottom) ("bottom type")
+    lazy val intersection: P[Type] = l((typ1 ~ (("&" | "/\\" | "∧") ~> typ2)) ^^ Intersect) ("intersection type")
+    lazy val union: P[Type] = l((typ2 ~ (("|" | "\\/" | "∨") ~> typ3)) ^^ Union) ("union type")
+    lazy val dcl: Parser[Dcl] =
+      l(methodLabel ~ (":" ~> typ ~ (("->" | "→") ~> typ)) ^^ {case l~(s~u) => MethodDecl(l, ArrowType(s, u))}) ("method declaration") |
+      l(typeLabel ~ (":" ~> typ ~ (".." ~> typ)) ^^ {case l~(s~u) => TypeDecl(l, TypeBounds(s, u))}) ("type declaration") |
+      l(valueLabel ~ (":" ~> typ) ^^ {case l~u => ValueDecl(l, u)}) ("value declaration")
+    def refinement(typ: => Parser[Type]) = l(
+      typ ~ (("{" ~> bind(ident) <~ ("=>" | "⇒")) >> {z => under(z){p =>
+        only[List[Dcl]](repsep(p.dcl, opt(";")), uniqLabels, _ => "duplicate declaration") ^^ Decls }} <~ "}") ^^
+      { case ty~ds => Refine(ty, ds) }) ("refinement type")
+    lazy val type_selection: Parser[Type] = l(
+      only[Term](term, _.isPath, "expected path, not arbitrary term like "+_) ~ ("." ~> typeLabel) ^^ Tsel
+    ) ("type selection")
+    lazy val typ1: P[Type] =
+      refinement(typ1) |
+      type_selection |
+      top |
+      bottom |
+      l("(" ~> typ <~ ")") ("parenthesized type")
+    lazy val typ2: P[Type] = intersection | typ1
+    lazy val typ3: P[Type] = union | typ2
+    lazy val typ: P[Type] = refinement(typ3) | typ3
+    lazy val concrete_typ: P[Type] = only[Type](typ, _.isConcrete, "expected concrete type, unlike "+_)
+  }
+  object Parser extends BindingParser(HashMap.empty)
+}
---
+++ src/scala/src/test/scala/dot/TestDotParser.scala
@@ -0,0 +1,82 @@
+package dot
+
+import org.scalatest._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class TestDotParser extends Suite with DotParsing {
+  import Terms._
+  import Types._
+  import Members._
+  
+  def parse[T](p: Parser[T])(in: String) = phrase(p)(new lexical.Scanner(in))
+
+  def ok[T](p: Parser[T], expected: Option[T] = None)(in: String) = parse(p)(in) match {
+    case Success(actual, _) => expected.foreach(e => expect(e)(actual))
+    case _@r => fail("expected success, got " + r)
+  }
+  def bad[T](p: Parser[T], expectedMsg: Option[String] = None)(in: String) = parse(p)(in) match {
+    case Failure(msg, _) => expectedMsg.foreach(e => expect(e)(msg))
+    case _@r => fail("expected failure, got " + r)
+  }
+    
+  def testValueLabel1() = ok(Parser.valueLabel, Some(ValueLabel("foo")))("foo")
+  def testValueLabelBad1() = bad(Parser.valueLabel, Some("value label should start with a lower case, unlike Foo"))("Foo")
+
+  def testNew1() = ok(Parser.term)("val x = new Top; x")
+  def testNew2() = ok(Parser.term)("val x = new Top\nx")
+  def testNew3() = ok(Parser.term)("val x = new Top(); x")
+  def testNew4() = ok(Parser.term)("val x = new Top(l=x); x")
+  def testNew5() = ok(Parser.term)("val x = new Top(l=x; m(x)=x); x")
+  def testNew6() = ok(Parser.term)("val x = new Top(l=x; m(x)=(val y = new Top; x)); x")
+  def testNewBad1() = bad(Parser.term, Some("expected concrete type, unlike Bottom"))("val x = new Bot; x")
+
+  def testSel1() = ok(Parser.term)("val x = new Top; x.l")
+  def testSel2() = ok(Parser.term)("val x = new Top; x.a.b")
+
+  def testMsel1() = ok(Parser.term)("val x = new Top; x.m(x)")
+  def testMsel2() = ok(Parser.term)("val x = new Top; x.a.b(x.a)")
+
+  def testIntersection1() = ok(Parser.typ)("Top & Top")
+  def testUnion1() = ok(Parser.typ)("Top | Top")
+  def testTypeGrouping1() = ok(Parser.typ, Some(Union(Intersect(Top, Top), Top)))("Top & Top | Top")
+  def testTypeGrouping2() = ok(Parser.typ, Some(Union(Top, Intersect(Top, Top))))("Top | Top & Top")
+  
+  def testConcreteType1() = ok(Parser.concrete_typ)("Top")
+  def testConcreteType2() = ok(Parser.concrete_typ)("Top & Top")
+  def testConcreteTypeBad1() = bad(Parser.concrete_typ, Some("expected concrete type, unlike Union(Top,Top)"))("Top | Top")
+
+  def testRefine1() = ok(Parser.typ)("Top { z => }")
+  def testRefine2() = ok(Parser.typ)(
+      """
+      Top { z =>
+         L: Bottom..Top
+        !L: Bottom..Top
+         m: Top -> Top
+         l: Top
+      }
+      """
+  )
+  def testRefine3() = ok(Parser.typ)("Top { z => a: Top; b: Top { z => c: Top } }")
+
+  val tyr = Refine(Top, Name("z")\\(Decls(List())))
+  def testTypeGrouping3() = ok(Parser.typ, Some(Union(Intersect(tyr,tyr),tyr)))("Top { z => } & Top { z => } | Top { z => }")
+  def testTypeGrouping4() = ok(Parser.typ, Some(Union(Intersect(tyr,Top),Top)))("Top { z => } & Top | Top")
+
+  def testUniqLabels1() = bad(Parser.term, Some("duplicate definition"))("val x = new Top(l=x;l=x); x")
+  def testUniqLabels2() = bad(Parser.typ, Some("duplicate declaration"))("Top { z => l: Top; l: Top }")
+
+  def test1() = ok(Parser.term)(
+      """
+      val x = new ⊤ { x ⇒
+        !L : ⊥..⊤
+         L : ⊥..⊤
+      }
+      val y = new x.!L
+      val id = new ⊤ { id ⇒
+        apply : ⊤→⊤
+      }(apply(x)=x)
+      id.apply(y)
+      """)
+}
