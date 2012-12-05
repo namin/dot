86f71d5a178b7d072558bbf672f6ced603ed0e35
Nada Amin <namin@alum.mit.edu>
2012-12-05 09:55:12 +0100

    baby steps

--- src/coq/Dot_Theorems.v
+++ src/coq/Dot_Theorems.v
@@ -12,6 +12,22 @@ Require Import Coq.Logic.Decidable.
 
 (* ********************************************************************** *)
 
+Lemma tp_sel__expands_fix : forall p L E U,
+  exists DT,  expands_fix (tp_sel p L) (decls_fin nil) nil E U DT /\ decls_ok DT.
+Proof.
+  intros p L E U.
+  assert (exists DS, expands_iter ((tp_sel p L ~ (decls_fin nil)) ++ nil) E U DS /\ decls_ok DS) as Hfix1.
+    (* TODO *) skip.
+  assert (forall P, P \/ ~P) as HMid. skip.
+  inversion Hfix1 as [DS [HDSex HDSok]].
+  specialize (HMid (decls_fin nil = DS)). inversion HMid as [Heq | Hneq].
+  exists DS. rewrite Heq. split. apply expands_fix_one. rewrite Heq in HDSex. assumption. assumption.
+  assert (exists DSf, expands_fix (tp_sel p L) DS nil E U DSf /\ decls_ok DSf) as Hfix2.
+    (* TODO *) skip.
+  inversion Hfix2 as [DSf [HDSfex HDSfok]].
+  exists DSf. split. apply expands_fix_rec with (DSA:=DS). assumption. assumption. assumption. assumption.
+Qed.
+
 Theorem tp_wf__expands : forall E T,
   wf_env E -> wf_tp E T -> exists DT, E |= T ~< DT.
 Proof.
@@ -25,16 +41,10 @@ Proof.
     apply expands_any. apply expands_iter_rfn with (DSP:=DT). inversion HDT; subst; assumption. assumption.
   Case "sel".
     assert (exists DT,  expands_fix (tp_sel p L) (decls_fin nil) nil E U DT /\ decls_ok DT) as Hfix.
-      skip. (* TODO *)
+      apply tp_sel__expands_fix; auto.
     inversion Hfix as [DT [HDTfix HDTok]].
     exists DT.
-    apply expands_any. apply expands_iter_tsel_fix with (S:=S) (U:=U).
-      assumption.
-      assumption.
-      auto.
-      assumption.
-      assumption.
-      assumption.
+    apply expands_any. apply expands_iter_tsel_fix with (S:=S) (U:=U); auto.
   Case "and".
     specialize (IHHwf1 Henv). inversion IHHwf1 as [DT1 HDT1].
     specialize (IHHwf2 Henv). inversion IHHwf2 as [DT2 HDT2].
